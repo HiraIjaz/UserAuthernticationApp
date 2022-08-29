2c458ce11613b21f5c24a30fbc74ad8e9acc6b16
hira ijaz <68460038+HiraIjaz@users.noreply.github.com>
2022-08-30 00:17:00 +0500

    Update views.py

--- userAuthentication/views.py
+++ userAuthentication/views.py
@@ -10,43 +10,43 @@ from django.http import HttpResponseRedirect
 
 
 def homepage(request):
-    return render(request, "userAuthentication/homepage.html")
+    return render(request, 'userAuthentication/homepage.html')
 
 
 def signin(request):
-    if request.method == "POST":
-        username = request.POST["username"]
+    if request.method == 'POST':
+        username = request.POST['username']
         password = request.POST['pass1']
 
         user = authenticate(username=username, password=password)
 
         if user is not None:
             login(request, user)
-            fname= user.first_name
-            return render(request, "userAuthentication/base.html", {'fname': fname})
-
+            fname = user.first_name
+            return render(request, 'userAuthentication/base.html',
+                          {'fname': fname})
         else:
-            messages.error(request, "Incorrect credentials Entered")
+
+            messages.error(request, 'Incorrect credentials Entered')
             return redirect('homepage')
 
-    return render(request, "userAuthentication/signin.html")
+    return render(request, 'userAuthentication/signin.html')
 
 
 def signout(request):
     logout(request)
-    messages.success(request, "User logged out!")
+    messages.success(request, 'User logged out!')
     return redirect('homepage')
 
 
-
 def signup(request):
-    if request.method == "POST":
-        username = request.POST["username"]
-        lname = request.POST["lname"]
-        fname = request.POST["fname"]
-        email = request.POST["email"]
-        pass1 = request.POST["pass1"]
-        pass2 = request.POST["pass2"]
+    if request.method == 'POST':
+        username = request.POST['username']
+        lname = request.POST['lname']
+        fname = request.POST['fname']
+        email = request.POST['email']
+        pass1 = request.POST['pass1']
+        pass2 = request.POST['pass2']
         if pass1 == pass2:
             if User.objects.filter(username=username).exists():
                 messages.error(request, 'Username is already taken!\n')
@@ -55,13 +55,15 @@ def signup(request):
                 messages.error(request, 'Email already taken!')
                 return redirect('signup')
             else:
-                user = User.objects.create_user(username=username, password=pass1, email=email,
-                                                first_name=fname, last_name=lname)
+                user = User.objects.create_user(username=username,
+                        password=pass1, email=email, first_name=fname,
+                        last_name=lname)
                 user.save()
-                messages.success(request, "Account Created Successfully!")
+                messages.success(request,
+                                 'Account Created Successfully!')
                 return redirect('signin')
         else:
             messages.error(request, 'password do not match! \n')
             return redirect('signup')
     else:
-        return render(request, "userAuthentication/signup.html")
+        return render(request, 'userAuthentication/signup.html')
